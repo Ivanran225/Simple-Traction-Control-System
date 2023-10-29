b8046622c30bcb0986326c275a8990020a9e699b
Ivanran225 <ivanr.an225@gmail.com>
2023-10-29 13:08:50 -0300

    New Approach
    Throttle input is now a global variable that is updated by the slider

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,7 @@
+// Import required libraries
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 // Define the pins for rear and front wheel speed sensors
 const int rearSensorPin = 2;  // Use the appropriate pin number
@@ -13,70 +15,110 @@ int targetSpeed = 100; // Adjust to your specific needs
 const int speedDifferenceThreshold = 10; // Adjust as needed
 bool slipDetected = false;
 
-// Set up Wi-Fi credentials
+// Replace with your network credentials
 const char* ssid = "Galaxy";
 const char* password = "esp32abc";
 
-ESP8266WebServer server(80);
+const int output = 2;
 
-void setup() {
+String sliderValue = "0";
+
+const char* PARAM_INPUT = "value";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ESP Web Server</title>
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 2.3rem;}
+    p {font-size: 1.9rem;}
+    body {max-width: 400px; margin:0px auto; padding-bottom: 25px;}
+    .slider { -webkit-appearance: none; margin: 14px; width: 360px; height: 25px; background: #FFD65C;
+      outline: none; -webkit-transition: .2s; transition: opacity .2s;}
+    .slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 35px; height: 35px; background: #003249; cursor: pointer;}
+    .slider::-moz-range-thumb { width: 35px; height: 35px; background: #003249; cursor: pointer; } 
+  </style>
+</head>
+<body>
+  <h2>ESP Web Server</h2>
+  <p><span id="textSliderValue">%SLIDERVALUE%</span></p>
+  <p><input type="range" onchange="updateSliderPWM(this)" id="pwmSlider" min="0" max="1023" value="%SLIDERVALUE%" step="1" class="slider"></p>
+<script>
+function updateSliderPWM(element) {
+  var sliderValue = document.getElementById("pwmSlider").value;
+  document.getElementById("textSliderValue").innerHTML = sliderValue;
+  console.log(sliderValue);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/slider?value="+sliderValue, true);
+  xhr.send();
+}
+</script>
+</body>
+</html>
+)rawliteral";
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if (var == "SLIDERVALUE"){
+    return sliderValue;
+  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  
+  pinMode(rearSensorPin, INPUT);
+  pinMode(frontSensorPin, INPUT);
+  pinMode(rearMotorPin1, OUTPUT);
+  
+  analogWrite(output, sliderValue.toInt());
+
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi...");
+    Serial.println("Connecting to WiFi..");
   }
-  Serial.println("Connected to WiFi");
 
-  // Define the control page with a vertical slider and square for slip
-  server.on("/", HTTP_GET, [](){
-    
-String page = "<html><head>";
-page += "<style>";
-page += ".slider { transform: rotate(270deg); }";
-page += ".square { width: 50px; height: 50px; background-color: ";
-page += slipDetected ? "red;" : "green;";
-page += " animation: blink 1s infinite; }";
-page += "@keyframes blink {";
-page += "  0% { opacity: 0; }";
-page += "  50% { opacity: 1; }";
-page += "  100% { opacity: 0; }";
-page += "}";
-page += ".content {";
-page += "  margin-top: 20px;"; // Adjust the margin as needed
-page += "}";
-page += "</style></head><body>";
-page += "<h1>RC Car Control</h1>";
-page += "<div class='content'>";
-page += "<p>Target Speed: " + String(targetSpeed) + "</p>";
-page += "<p>Current Speed: " + String(analogRead(rearSensorPin)) + "</p>";
-page += "<p>Slip Detected: <div class='square'></div></p>";
-page += "</div>";
-page += "<input type='range' class='slider' id='speedSlider' min='0' max='255' oninput='updateSpeed(value)' onload='setSliderValue()'>";
-page += "<script>";
-page += "function updateSpeed(val) {";
-page += "  document.getElementById('speedSlider').value = val;";
-page += "}";
-page += "function setSliderValue() {";
-page += "  document.getElementById('speedSlider').value = " + String(targetSpeed) + ";";
-page += "}";
-page += "</script>";
-page += "</body></html>";
-server.send(200, "text/html", page);
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
 
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
   });
 
-  // Start the server
+  // Send a GET request to <ESP_IP>/slider?value=<inputMessage>
+  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
+    if (request->hasParam(PARAM_INPUT)) {
+      inputMessage = request->getParam(PARAM_INPUT)->value();
+      sliderValue = inputMessage;
+      analogWrite(output, sliderValue.toInt());
+    }
+    else {
+      inputMessage = "No message sent";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  });
+  
+  // Start server
   server.begin();
-
-  pinMode(rearSensorPin, INPUT);
-  pinMode(frontSensorPin, INPUT);
-  pinMode(rearMotorPin1, OUTPUT);
 }
-
+  
 void loop() {
-  server.handleClient(); // Handle web server requests
-
+  Serial.println(sliderValue);
+  
   // Read rear and front wheel speed sensor values
   int rearSpeed = analogRead(rearSensorPin);
   int frontSpeed = analogRead(frontSensorPin);
