1762ef16337131d88b9a41449258050718abcce9
REFOJO IVAN TOBIAS <46585955@ort>
2023-11-14 14:58:07 -0300

    Install next

---
+++ Web Server/test1.tsx
@@ -0,0 +1,6 @@
+<button type="button" className="bg-blue-900 w-48 h-48 flex justify-center items-center rounded-full">
+    <div className="bg-red-500 w-16 h-16 rounded-full flex justify-center items-center border-2 border-white">
+      <span className="text-white text-3xl font-bold">10</span>
+    </div>
+    <div className="w-full h-1/4 bg-gray-200 rounded-b"></div>
+  </button>
