3083bf41a1830bce45e7a737f724713815f700bc
Ivanran225 <101880642+Ivanran225@users.noreply.github.com>
2023-11-15 17:26:45 -0300

    .

--- web_server/app/page.tsx
+++ web_server/app/page.tsx
@@ -1,16 +1,17 @@
+import Image from 'next/image'
+
 export default function ondas() {
   return (
     <button
-      className="bg-gray-600 border-4 border-black rounded-lg p-5 text-white inline-block font-bold text-2xl text-center cursor-pointer transition ease-in-out delay-150 duration-300"
-      style={{
-        boxShadow: "8px 8px 0px 0px #000000",
-        ':hover': {
-          backgroundColor: '#4a5568', // Replace with your hover color
-          transform: 'translateY(1px)', // Adjust the amount of translation
-        }
-      }}
-    >
+      className="
+      bg-gray-600 border-4 border-black rounded-lg p-5 
+      text-white inline-block font-bold text-2xl text-center cursor-pointer 
+      transition ease-in-out delay-150 
+      hover:bg-gray-500 
+      hover:translate-y-1 
+      hover:scale-100 hover:bg-indigo-500 duration-300 ..."
+      style={{ boxShadow: "8px 8px 0px 0px #000000" }}>
       Click
     </button>
-  );
+  )
 }
