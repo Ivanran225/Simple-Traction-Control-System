51c4e7f325ef38f86eb98fc1193e67986de711e1
Ivanran225 <ivanr.an225@gmail.com>
2023-11-15 23:10:44 -0300

    Implemented Neumorphism Style for Button Container

--- web_server/app/page.tsx
+++ web_server/app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 
 export default function Button() {
   return (
-    <div className="button-container">
+  <div className="button-container">
+    <div className="button-box">
       <button className="button">
         SHALOM
       </button>
@@ -13,34 +14,46 @@ export default function Button() {
       <button className="button">
         SHALOM
       </button>
-      <style jsx>{`
-        .button-container {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100vh;
-          background-color: #223848;
-          flex-direction: column;
-          gap: 20px;
-        }
-        .button {
-          background-color: #324858;;
-          border-radius: 50px;
-          padding: 20px;
-          width: 400px;
-          color: orange; // Changed color to orange
-          display: inline-block;
-          font-size: 20px;
-          text-align: left; // Aligned text to the left
-          cursor: pointer;
-          transition: background-color 0.3s ease, box-shadow 0.3s ease, transform 0.3s ease; // Added transition for transform
-          box-shadow: 12px 12px 10px 2px #000000, inset -2px -2px 5px #000000, inset 1px 1px 5px #ffffff; 
-        }
-        .button:active {
-          box-shadow: 7px 7px 10px 1px #000000, inset -1px -1px 5px #000000, inset 2px 2px 5px #ffffff;
-          transform: scale(0.99);
-        }
-`}</style>
+    </div>
+    <style jsx>{`
+      .button-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #122838;
+        flex-direction: column;
+        gap: 20px;
+      }
+      .button-box {
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+        padding: 30px;
+        background-color: #223848; // Light grey background
+        border-radius: 40px;
+        background: #223848;
+        box-shadow:  29px 29px 30px #1d303d,
+                   -29px -29px 30px #274053;
+      }
+      .button {
+        background-color: #324858;;
+        border-radius: 50px;
+        padding: 20px;
+        width: 400px;
+        color: orange;
+        display: inline-block;
+        font-size: 20px;
+        text-align: left;
+        cursor: pointer;
+        transition: background-color 0.3s ease, box-shadow 0.3s ease, transform 0.3s ease;
+        box-shadow: 12px 12px 10px 2px #000000, inset -2px -2px 5px #000000, inset 1px 1px 5px #ffffff; 
+      }
+      .button:active {
+        box-shadow: 7px 7px 10px 1px #000000, inset -1px -1px 5px #000000, inset 2px 2px 5px #ffffff;
+        transform: scale(0.95);
+      }
+    `}</style>
     </div>
   )
 }
