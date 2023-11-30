aa018c3891560eca88c157875c3d8fa36d5332c8
Ivanran225 <ivanr.an225@gmail.com>
2023-11-29 22:57:27 -0300

    Solving Motor Problems

--- Arduinos/Speed_calculator/Speed_calculator.ino
+++ Arduinos/Speed_calculator/Speed_calculator.ino
@@ -1,7 +1,7 @@
 const int sensorPin = A0;
 const int triggerThresholdLow = 200;
 const int triggerThresholdHigh = 800;
-int throtle = 0;
+int throtle;
 const int enablePin = 9;   // PWM pin connected to ENA on the L298N
 const int in1Pin = 8;      // Input 1 pin connected to IN1 on the L298N
 const int in2Pin = 7;      // Input 2 pin connected to IN2 on the L298N
@@ -29,9 +29,28 @@ void setup() {
   previousSensorValue = analogRead(sensorPin);  // Initialize previousSensorValue
 }
 
-void loop() {
-  int sensorValue = analogRead(sensorPin);
 
+int smoothValue(int rawValue) {
+  // Subtract the oldest reading from the total
+  total = total - readings[index];
+  
+  // Add the new reading to the total
+  total = total + rawValue;
+
+  // Store the new reading in the array
+  readings[index] = rawValue;
+
+  // Move to the next index
+  index = (index + 1) % numReadings;
+
+  // Calculate the average
+  int average = total / numReadings;
+
+  return average;
+}
+void speed_sensor(){
+
+  int sensorValue = analogRead(sensorPin);
   // Update the previous sensor value
   previousSensorValue = sensorValue;
 
@@ -57,32 +76,22 @@ void loop() {
   // Update the previous sensor value
   previousSensorValue = sensorValue;
 
-  if (Serial.available() > 0) {
-    throtle = Serial.parseInt();
-
-    // Ensure the speed value is within the valid range (0 to 100)
-    speed = constrain(throtle, 0, 100);
-
-    // Map the speed value to the PWM range (0 to 255)
-    int pwmValue = map(throtle, 0, 100, 0, 255);
-
-    // Set the motor speed
-    analogWrite(enablePin, pwmValue);
-
-    // Control the motor direction based on the sign of the speed
-    if (throtle > 0) {
-      digitalWrite(in1Pin, HIGH);
-      digitalWrite(in2Pin, LOW);
-    } else if (throtle < 0) {
-      digitalWrite(in1Pin, LOW);
-      digitalWrite(in2Pin, HIGH);
-    } else {
-      // Stop the motor if speed is 0
-      digitalWrite(in1Pin, LOW);
-      digitalWrite(in2Pin, LOW);
-    }
-  }
 }
+void motors() {
+
+  if (Serial.available()) {
+    throtle = Serial.parseInt();
+  }
+  // Ensure the speed value is within the valid range (0 to 100)
+  speed = constrain(throtle, 0, 100);
+
+  //int pwmValue = map(throtle, 0, 100, 0, 255);
+  // Set the motor speed
+  analogWrite(enablePin, 0);
+  digitalWrite(in1Pin, HIGH);
+  digitalWrite(in2Pin, HIGH);
+}
+void basora(){
   // Add any other code you need to run in the loop
 
   /*
@@ -91,42 +100,30 @@ void loop() {
 
   // Check if the sensor is triggered (from 1023 to 0)
   if (sensorValue < 200 && previousSensorValue > 800) {
-  // Record the start time when the sensor is triggered
-  startTime = millis();
-}
+    // Record the start time when the sensor is triggered
+    startTime = millis();
+  }
 
-// Check if the sensor is untriggered (from more than 800 to less than 200)
-if (sensorValue > 800 && previousSensorValue < 200) {
-  // Record the end time when the sensor is untriggered
-  //endTime = millis();
+    // Check if the sensor is untriggered (from more than 800 to less than 200)
+  if (sensorValue > 800 && previousSensorValue < 200) {
+    // Record the end time when the sensor is untriggered
+    //endTime = millis();
 
-  // Calculate the speed (change in distance / time)
-  // Note: You may need to calibrate this formula based on your specific setup
-  speed = endTime - startTime;
-  smoothedValue = smoothValue(speed);
-  // Print the smoothed value
-  Serial.println(smoothedValue);
-}
+    // Calculate the speed (change in distance / time)
+    // Note: You may need to calibrate this formula based on your specific setup
+    speed = endTime - startTime;
+    smoothedValue = smoothValue(speed);
+    // Print the smoothed value
+    Serial.println(smoothedValue);
 
   // Update the previous sensor value
-  previousSensorValue = sensorValue;
-}
+    previousSensorValue = sensorValue;
+
 */
-int smoothValue(int rawValue) {
-  // Subtract the oldest reading from the total
-  total = total - readings[index];
-  
-  // Add the new reading to the total
-  total = total + rawValue;
+}
 
-  // Store the new reading in the array
-  readings[index] = rawValue;
-
-  // Move to the next index
-  index = (index + 1) % numReadings;
-
-  // Calculate the average
-  int average = total / numReadings;
-
-  return average;
+void loop() {
+  //speed_sensor();
+  motors();
+  Serial.println(throtle);  
 }
